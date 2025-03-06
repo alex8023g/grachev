f1c1fa9ce09ddcddf98b546f442ec7ce88f5540b
alex8023 <alex8023@yandex.ru>
2025-03-06 15:49:32 +0300

    wrap minio reqs to try catch

--- src/app/api/minio/streamvideo/route.ts
+++ src/app/api/minio/streamvideo/route.ts
@@ -7,30 +7,34 @@ export async function GET(req: Request) {
 
   const objectName = decodeURI(new URL(req.url).search).substring(1);
 
-  const stats = await minioClient.statObject(bucket, objectName);
+  try {
+    const stats = await minioClient.statObject(bucket, objectName);
 
-  const videoSize = stats.size;
-  const start = Number(range?.split('=')[1].split('-')[0]);
-  const end = Number(range?.split('=')[1].split('-')[1]) || videoSize - 1;
-  const contentLength = end - start === 1 ? 1 : end - start + 1; // для ios и macos
+    const videoSize = stats.size;
+    const start = Number(range?.split('=')[1].split('-')[0]);
+    const end = Number(range?.split('=')[1].split('-')[1]) || videoSize - 1;
+    const contentLength = end - start === 1 ? 1 : end - start + 1; // для ios и macos
 
-  const res = await minioClient.getPartialObject(
-    bucket,
-    objectName,
-    start,
-    end,
-  );
-  const data: ReadableStream = iteratorToStream(nodeStreamToIterator(res));
+    const res = await minioClient.getPartialObject(
+      bucket,
+      objectName,
+      start,
+      end,
+    );
+    const data: ReadableStream = iteratorToStream(nodeStreamToIterator(res));
 
-  return new Response(data, {
-    status: 206,
-    headers: {
-      'Accept-Ranges': `bytes`,
-      'Content-Length': `${contentLength}`,
-      'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-      'Content-Type': stats.metaData['Content-Type'],
-    },
-  });
+    return new Response(data, {
+      status: 206,
+      headers: {
+        'Accept-Ranges': `bytes`,
+        'Content-Length': `${contentLength}`,
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Content-Type': stats.metaData['Content-Type'],
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function* nodeStreamToIterator(stream: Readable) {
