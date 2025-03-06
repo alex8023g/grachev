0f11d4f92a9015850a28c961d22c441a4c0f240f
alex8023 <alex8023@yandex.ru>
2025-03-06 11:20:53 +0300

    upd myAppsPage

--- src/app/myapps/page.tsx
+++ src/app/myapps/page.tsx
@@ -6,28 +6,49 @@ export default async function MyAppsPage() {
       name: 'Diary web',
       img: 'diaryWeb.png',
       url: 'https://simplediary.io',
+      description: 'NextJS, Postgres, Prisma ORM, Next Auth, Tailwind, Shadcn',
+      github: 'https://github.com/alex8023g/diary-1/tree/main',
     },
     {
       name: 'Diary android',
       img: 'diaryAndroid.png',
       url: 'https://play.google.com/store/apps/details?id=diary.app',
+      description:
+        'Android приложение, в России открывать с android телефона или через vpn',
+      github: 'https://github.com/alex8023g/diary-app',
     },
     {
       name: 'Todooooit android',
       img: 'todooooit.png',
       url: 'https://play.google.com/store/apps/details?id=todo.cap.v1.com',
+      description:
+        'Android приложение, в России открывать с android телефона или через vpn',
+      github: 'https://github.com/alex8023g/todo-cap-1',
     },
     {
       name: 'Pomodoro',
       img: 'pomodoro.png',
       url: 'https://pomodoro-alpha-six.vercel.app',
+      description: 'React, Typescript, React router, DnD, MUI',
+      github: 'https://github.com/alex8023g/pomodoro',
     },
-  ];
+    {
+      name: 'W-wave',
+      img: 'radio.png',
+      url: 'https://w-wave-six.vercel.app',
+      description: 'адаптивная верстка pixel-perfect',
+      github: '',
+    },
+  ] as const;
+
   return (
     <>
       <ul className='flex flex-wrap'>
         {apps.map((app) => (
-          <li key={app.name} className=''>
+          <li
+            key={app.name}
+            className='mb-3 mr-3 flex flex-col overflow-hidden rounded-lg border shadow-md'
+          >
             <a
               href={app.url}
               target='_blank'
@@ -40,6 +61,19 @@ export default async function MyAppsPage() {
                 alt='Picture of the author'
               />
             </a>
+            <div className='flex-grow bg-slate-200 p-3'>
+              <div className='max-w-[470px]'>
+                <span className='font-semibold'>Description:&nbsp;</span>
+                <span className='text-wrap'>{app.description}</span>
+              </div>
+              <div>
+                {app.github && (
+                  <a href={app.github} target='_blank'>
+                    Github
+                  </a>
+                )}
+              </div>
+            </div>
           </li>
         ))}
       </ul>
