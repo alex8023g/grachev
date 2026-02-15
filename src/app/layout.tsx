e9622aa9e14ab5a52eee96c711b1a53f3a4cdaba
alex8023 <alex8023@yandex.ru>
2026-02-15 10:29:18 +0300

    add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,7 @@ import './globals.css';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { SidebarBlock } from '@/components/forSidebar/SidebarBlock';
 import { Header } from '@/components/Header';
-// import { YaMetrika } from '@/components/YaMetrika';
-
+import { YandexMetricaProvider } from 'next-yandex-metrica';
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
@@ -30,19 +29,29 @@ export default function RootLayout({
   console.log('RootLayout');
   return (
     <html lang='en'>
-      {/* <YaMetrika /> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SidebarProvider>
-          <SidebarBlock />
-          <div className='flex h-dvh w-full flex-col overflow-hidden'>
-            <Header />
-            <main className='flex h-full flex-col overflow-y-auto p-3'>
-              {children}
-            </main>
-          </div>
-        </SidebarProvider>
+        <YandexMetricaProvider
+          tagID={106835902}
+          initParameters={{
+            webvisor: true,
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+          }}
+          router='app'
+        >
+          <SidebarProvider>
+            <SidebarBlock />
+            <div className='flex h-dvh w-full flex-col overflow-hidden'>
+              <Header />
+              <main className='flex h-full flex-col overflow-y-auto p-3'>
+                {children}
+              </main>
+            </div>
+          </SidebarProvider>
+        </YandexMetricaProvider>
       </body>
     </html>
   );
